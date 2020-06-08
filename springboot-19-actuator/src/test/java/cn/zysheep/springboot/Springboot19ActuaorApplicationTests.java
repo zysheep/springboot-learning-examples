37de54f6d43a7cbaf611fe4e055ed704a5696099
zysheep <zysheep@126.com>
2020-06-08 10:21:27 +0800

    done springboot-actuator==>SpringBoot监控管理

---
+++ springboot-19-actuator/src/test/java/cn/zysheep/springboot/Springboot19ActuaorApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zysheep.springboot;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @version v1.0.0
+ * @ProjectName: springboot-learning-examples
+ * @ClassName: Springboot19ActuaorApplicationTests
+ * @Author: 三月三
+ */
+@SpringBootTest
+public class Springboot19ActuaorApplicationTests {
+}
