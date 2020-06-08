4ac1e199a63853d17e02a76bd3fd074260c485a4
zysheep <zysheep@126.com>
2020-06-08 17:16:02 +0800

    done springboot-nacos==>加入nacos服务注册中心的restful调用

---
+++ springboot-20-nacos/nacos-restful-consumer/src/main/java/cn/zysheep/springboot/SpringRestConsumerApplication.java
@@ -0,0 +1,17 @@
+package cn.zysheep.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version v1.0.0
+ * @ProjectName: springboot-learning-examples
+ * @ClassName: SpringRestConsumerApplication
+ * @Author: 三月三
+ */
+@SpringBootApplication
+public class SpringRestConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestConsumerApplication.class,args);
+    }
+}
---
+++ springboot-20-nacos/nacos-restful-consumer/src/main/java/cn/zysheep/springboot/controller/RestConsumerController.java
@@ -0,0 +1,52 @@
+package cn.zysheep.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+/**
+ * @version v1.0.0
+ * @ProjectName: springboot-learning-examples
+ * @ClassName: RestConsumerController
+ * @Author: 三月三
+ */
+@RestController
+public class RestConsumerController {
+
+    //服务id即注册中心的中的服务名
+    private String serviceId="nacos-restful-provider";
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Value("${provider.address}")
+    private String providerAddress;
+
+
+    @GetMapping(value = "/service")
+    public String service(){
+        RestTemplate restTemplate = new RestTemplate();
+        //调用服务
+        String providerResult = restTemplate.getForObject("http://" + providerAddress + "/service",String.class);
+        return "consumer invoke | " + providerResult;
+    }
+
+    @GetMapping(value = "/service2")
+    public String service2(){
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ServiceInstance instance = loadBalancerClient.choose(serviceId);
+        URI uri = instance.getUri();
+        System.out.println(uri);
+        //调用服务
+        String providerResult = restTemplate.getForObject(uri+ "/service",String.class);
+        return "consumer invoke | " + providerResult+"port:"+uri.getPort();
+    }
+}
